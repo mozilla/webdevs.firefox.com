59004913518e40c54d9ac0bbda9dfc20d964d347
Jake Archibald <jaffathecake@gmail.com>
2026-08-21 13:54:59 +0100

    Layout & head component

--- astro.config.ts
+++ astro.config.ts
@@ -4,5 +4,7 @@ import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://webdevs.firefox.com',
+  trailingSlash: 'always',
   integrations: [preact()],
 });
